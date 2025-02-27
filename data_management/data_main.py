7c6622432559231a078a26ead229b385f1504b7a
hayleepierce <pierce03@allegheny.edu>
2025-02-27 13:29:25 -0500

    Add experimental code

--- data_management/data_main.py
+++ data_management/data_main.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 import os
 import json
+import csv
 import pandas as pd
 from dotenv_vault import load_dotenv
 from emotion_entries import emotion_entries
@@ -35,3 +36,31 @@ from noun_adjective_entries import noun_entries, adjective_entries
 # result = wrd.insert_many(emotion_entries)
 # result = wrd.insert_many(noun_entries)
 # result = wrd.insert_many(adjective_entries)
+
+""" Compare original ratings and updated ratings. """
+
+# Read data from a given csv file
+def read_csv(filename):
+    # Initialize lists
+    ratings = []
+
+    # Open the given file
+    with open(f"{filename}", "r") as file:
+        # Read the given file
+        csvfile = csv.reader(file)
+        # Iterate through the lines in the file
+        for line in csvfile:
+            # Remove & save the last item each line
+            y = line.pop(2)
+            if y is not None and y != "sentiment rating":
+                # Add the saved item to the all_y_trues list as floats
+                ratings.append(float(y))
+    return ratings
+
+
+orgin_ratings = read_csv("data/WRD_orgin.csv")
+updated_ratings = read_csv("data/WRD_updated.csv")
+
+# Find the difference
+for orgin, updated in zip(orgin_ratings, updated_ratings):
+    difference = updated - orgin
