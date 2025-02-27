bc7c7d11e6e555c0361b32f763b53bbb83b12ed9
hayleepierce <pierce03@allegheny.edu>
2025-02-27 13:09:26 -0500

    Add original ratings

---
+++ data_management/data_main.py
@@ -0,0 +1,37 @@
+from pymongo import MongoClient
+import os
+import json
+import pandas as pd
+from dotenv_vault import load_dotenv
+from emotion_entries import emotion_entries
+from noun_adjective_entries import noun_entries, adjective_entries
+
+""" Load original ratings. """
+
+# wrd_entries = []
+# for entry in emotion_entries:
+#     wrd_entries.append(entry)
+# for entry in noun_entries:
+#     wrd_entries.append(entry)
+# for entry in adjective_entries:
+#     wrd_entries.append(entry)
+
+# with open("WRD_orgin.json", "w") as file:
+#     json.dump(wrd_entries, file, indent=0)
+# with open("WRD_orgin.json", "r") as file:
+#     pd_data = pd.read_json(file)
+# pd_data.to_csv("WRD_orgin.csv")
+
+""" Add entries to MongoDB. """
+
+# load_dotenv()
+
+# connection_str = os.getenv("CONNECTION_STR")
+# client = MongoClient(connection_str)
+
+# db = client["WRD"]
+# wrd = db.words
+
+# result = wrd.insert_many(emotion_entries)
+# result = wrd.insert_many(noun_entries)
+# result = wrd.insert_many(adjective_entries)
